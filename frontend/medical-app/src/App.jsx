2e3f0f9e500609ac4c7e4fb3d0f6fd433b0f5737
npu6703 <phuonguyen672003@gmail.com>
2025-10-21 22:00:00 -0500

    add router for doctor in app.jsx

--- frontend/medical-app/src/App.jsx
+++ frontend/medical-app/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import clinicLogo from '/logo.jpg'
 import LandingPage from "./components/LandingPage.jsx";
 import PatientPortal from "./components/PatientPortal";
+import DoctorPortal from './components/doctor/DoctorPortal.jsx';
 import './App.css'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/patientportal" element={<PatientPortal />} />
+      <Route path="/doctorportal" element={<DoctorPortal />} />
     </Routes>
   );
 }
