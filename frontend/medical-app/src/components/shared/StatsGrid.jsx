42a72c89f3277669a1d3fade9ecaf1bb1029d6df
npu6703 <phuonguyen672003@gmail.com>
2025-11-19 11:05:15 -0600

    pull?

--- frontend/medical-app/src/components/shared/StatsGrid.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function StatsGrid({ children }) {
-  return <div className="stats-grid">{children}</div>;
-}
--- frontend/medical-app/src/components/shared/index.js
+++
@@ -1,5 +0,0 @@
-import WelcomeHeader from './WelcomeHeader';
-import StatCard from './StatCard';
-import StatsGrid from './StatsGrid';
-
-export { WelcomeHeader, StatCard, StatsGrid };
